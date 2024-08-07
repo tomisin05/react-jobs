470a87bd87466f5c6c8135a0596601fe249a8163
Tomisin05 <tomisinbadmus@gmail.com>
2024-08-07 15:28:36 -0400

    fix deploy

--- src/layouts/MainLayout.jsx
+++ src/layouts/MainLayout.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Outlet } from 'react-router-dom'
 import NavBar from '../components/NavBar'
-import { ToastContainer } from 'react-toastify';
+import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
 
